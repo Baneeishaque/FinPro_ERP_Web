3d95664c58e998230db3f7a582e8756e4c860061
asarudheen <59663616+asr-droid@users.noreply.github.com>
2020-04-30 23:03:13 +0530

    finpro begin

---
+++ application/views/staff/staff_index.php
@@ -0,0 +1,17 @@
+<!-- user type-staff root page -->
+<html>
+<body >    
+<div class="container" >
+
+
+<div class="row">
+
+	<a href="<?php echo  base_url('general/gstclient');   ?>"><div class="box  ">gst client</div></a>
+	<a href="<?php echo  base_url('general/pretty_cash'); ?>"><div class="box  ">pretty cash</div></a>
+	<a href="<?php echo  base_url('general/pancard');     ?>"><div class="box  ">pancard</div></a>
+	
+</div>
+
+</div>
+</body>
+</html>
